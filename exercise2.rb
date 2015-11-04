d8e70cd4fb542de500eb0029a010a46228fc8b81
Peter Jang <peter.x.jang@gmail.com>
2015-11-03 19:44:08 -0600

    completed exercise 2b, c, d

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,23 @@
 11.times do |count|
   puts count
 end
+
+# In plain English, describe to your group:
+# if answer != 5
+# do something so long as answer is any number other than 5
+
+# Imagine in your code, you have this line:
+# if result != true
+# and you replace it with this line:
+#   unless result == true
+# will the code run the same as before? Yes or No?
+# yes, played with it in IRB
+
+# Look at the conditional below. What value(s) can the variable ‘result’ be for it to output “HELLO!” What values will it not output? Experiment in Sublime:
+# unless result
+#   puts "HELLO!"
+# end 
+# only false and nil work here
+
+# Translate the following into ruby code. Run the program to make sure it works:
+# If Sam can cook more than 10 recipes and Sally speaks more than 5 languages, they should date. If Sam can make crepes or Sally can speak French, they should marry. 
